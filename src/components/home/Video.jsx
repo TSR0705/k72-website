2ecf2547ca12c6acbde724b5cc0fcfc9c8314802
Tanmay Singh <tanmaysingh8246@gmail.com>
2025-09-07 13:44:09 +0530

    updated few things for production ready code

--- src/components/home/Video.jsx
+++ src/components/home/Video.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React from 'react';
 
 const Video = () => {
   return (
     <div className='h-full w-full'>
-        <video className='h-full w-full object-cover' autoPlay loop muted src="../../../public/video.mp4"></video>
+      <video
+        className='h-full w-full object-cover'
+        autoPlay
+        loop
+        muted
+      >
+        <source src="/video.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
     </div>
-  )
-}
+  );
+};
 
-export default Video
+export default Video;
